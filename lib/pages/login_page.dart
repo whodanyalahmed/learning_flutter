4da0e643d5f3247e61200199aebff77b87719ade
whodanyalahmed <daniahmedkhatri@gmail.com>
2021-11-07 14:17:40 +0500

    Text field added

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -10,17 +12,75 @@ class LoginPage extends StatelessWidget {
         color: Colors.white,
         child: Column(
           children: [
-            SvgPicture.asset(
-              "assets/images/login_image.svg",
-              fit: BoxFit.scaleDown,
-            ),
+            SvgPicture.asset("assets/images/login_image.svg",
+                fit: BoxFit.cover, height: 200),
+            SizedBox(height: 20),
             const Text(
-              "Welcome! \n Login Page",
+              "Welcome!",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 23,
               ),
             ),
+            const Text(
+              "Login",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 23,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "Username",
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password",
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      child: const Text("Login"),
+                      style: TextButton.styleFrom(
+                        primary: Colors.white,
+                      ),
+                      onPressed: () {
+                        // ignore: avoid_print
+                        print("logged in...");
+                      }),
+                ],
+              ),
+            )
           ],
         ));
   }
